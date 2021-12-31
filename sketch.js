9d7aac037edc1af0578df9ef8a5ac8c8dbf3dc47
karthikpnair06 <81678223+karthikpnair06@users.noreply.github.com>
2021-12-31 20:44:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+var snowflakesImg;
+
+function preload(){
+  backgroundImg = loadImage("snow3.jpg");
+  snowflakesImg = loadImage ("snow4.webp");
+}
+
+function setup() {
+  var canvas = createCanvas(800,400);
+  
+
+
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(backgroundImg);
+snowflakesImg;
+
+  drawSprites();
+}
