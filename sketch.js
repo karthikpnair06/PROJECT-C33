8f2da0600a964741b95b0596b52f3d326fc3b0fc
karthikpnair06 <81678223+karthikpnair06@users.noreply.github.com>
2021-12-31 20:45:01 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,3 @@
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
-
 var engine, world;
 var backgroundImg;
 var snowflakesImg;
